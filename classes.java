1994dac46bef3fd1fed82c347a65a301d49ce327
Thurow <luana.thurow@dellteam.com>
2023-08-25 18:38:30 -0300

    adicionando método d

--- classes.java
+++ classes.java
@@ -4,6 +4,7 @@ public class Classes {
 
     public static void main(String[] args) {
         ArrayList<Integer> numeros = new ArrayList<>();
+        ArrayList<Integer> inputList = new ArrayList<>();
         numeros.add(1);
         numeros.add(2);
         numeros.add(3);
@@ -14,6 +15,35 @@ public class Classes {
         int ocorrencias = nOcorrencias(numeros, elementoProcurado);
 
         System.out.println("O elemento " + elementoProcurado + " ocorre " + ocorrencias + " vezes na lista.");
+
+        inputList.add(1);
+        inputList.add(2);
+        inputList.add(2);
+        inputList.add(3);
+        inputList.add(4);
+        inputList.add(4);
+        inputList.add(4);
+
+        ArrayList<Integer> repeatedElements = listRepeat(inputList);
+        System.out.println("Elementos Repetidos: " + repeatedElements);
+    }
+
+    public static ArrayList<Integer> listRepeat(ArrayList<Integer> l) {
+        ArrayList<Integer> repeatedList = new ArrayList<>();
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for (Integer num : l) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                repeatedList.add(entry.getKey());
+            }
+        }
+
+        return repeatedList;
+    }
     }
 
     public static int nOcorrencias(ArrayList<Integer> lista, Integer elemento) {
